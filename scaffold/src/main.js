00b501aa6f1eb8efd2be888af71eda3e762217a2
Joe Scherben <joe.scherben@informatics-studio.com>
2024-01-11 15:40:57 -0500

    Udpate scaffold example main module preload polyfill to async to avoid error

--- scaffold/src/main.js
+++ scaffold/src/main.js
@@ -1,3 +1,5 @@
-import "vite/modulepreload-polyfill";
+if (import.meta.env.MODE !== "development") {
+  import("vite/modulepreload-polyfill");
+}
 import "./scss/styles.scss";
 import "./js/index.js";
